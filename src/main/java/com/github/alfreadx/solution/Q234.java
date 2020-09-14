bf08f67f1b8bba452c686d264c764d4f70c6aeea
alfred <alfreadx@gmail.com>
2020-09-14 20:28:42 +0800

    add Q234 234. Palindrome Linked List

---
+++ src/main/java/com/github/alfreadx/solution/Q234.java
@@ -0,0 +1,59 @@
+package com.github.alfreadx.solution;
+
+public class Q234 {
+
+    /**
+     * the Solution class
+     */
+    static class Solution {
+        /**
+         * put the method in here
+         */
+        public boolean isPalindrome(ListNode head) {
+            if (head == null) return true;
+
+            ListNode middle = findMiddle(head);
+            ListNode tail = reverse(middle.next);
+            middle.next = null; // 斷開中間節點
+
+            while (head != null && tail != null) {
+                if (head.val != tail.val) {
+                    return false;
+                }
+
+                head = head.next;
+                tail = tail.next;
+            }
+
+            return true;
+        }
+
+        private ListNode findMiddle(ListNode head) {
+            // slow: 1 step per round; fast: 2 steps per round
+            ListNode slow = head, fast = head.next;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+
+        private ListNode reverse(ListNode head) {
+            if (head == null) return null;
+
+            ListNode prev = null, next;
+
+            while (head != null) {
+                next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+
+            return prev;
+        }
+    }
+
+}
---
+++ src/test/java/com/github/alfreadx/solution/Q234Test.java
@@ -0,0 +1,40 @@
+package com.github.alfreadx.solution;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Q234Test {
+
+
+    private static Stream<Arguments> getParameters() {
+        List<Integer> input = Arrays.asList(1, 2);
+        List<Integer> input2 = Arrays.asList(1, 2, 2, 1);
+
+        return Stream.of(
+                Arguments.of(ListNode.of(input), false),
+                Arguments.of(ListNode.of(input2), true),
+                Arguments.of(null, true)
+        );
+    }
+
+    //put cut(class under test) in here
+    Q234.Solution cut = new Q234.Solution();
+
+    @ParameterizedTest
+    @MethodSource("getParameters")
+    public void test(ListNode input, boolean expected) {
+        boolean actual = cut.isPalindrome(input);
+
+        System.out.println(actual);
+
+        assertEquals(expected, actual);
+    }
+
+}
