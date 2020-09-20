e9a4d3cacf62867524f158bfd7e2948b39a95e53
alfred <alfreadx@gmail.com>
2020-09-20 22:53:44 +0800

    調整幫助類的屬性可見度
    因幫助類已經移到其他套件下，故需要開放屬性可見度

--- src/main/java/com/github/alfreadx/solution/helper/ListNode.java
+++ src/main/java/com/github/alfreadx/solution/helper/ListNode.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class ListNode {
 
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
         next = null;
     }
--- src/main/java/com/github/alfreadx/solution/helper/Node.java
+++ src/main/java/com/github/alfreadx/solution/helper/Node.java
@@ -12,9 +12,9 @@ import java.util.stream.Collectors;
 @Data
 @EqualsAndHashCode(exclude = "random")
 public class Node {
-    int val;
-    Node next;
-    Node random;
+    public int val;
+    public Node next;
+    public Node random;
 
     public Node(int val) {
         this.val = val;
